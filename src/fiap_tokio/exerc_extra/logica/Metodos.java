f77056a6b4af73e5a20b4d7713fbb85a34b4db82
Miguel <945507756m@gmail.com>
2023-04-17 17:45:15 -0300

    add descrição do exercicio na documentação da classe

--- src/fiap_tokio/exerc_extra/logica/Metodos.java
+++ src/fiap_tokio/exerc_extra/logica/Metodos.java
@@ -1,5 +1,14 @@
 package fiap_tokio.exerc_extra.logica;
+/**
+ * 4. Pedir ao usuário a digitação de um número inteiro qualquer, exibir o que se pede:
+   a) O número digitado pelo usuário;
+   b) O seu sucessor;
+   c) O seu antecessor;
+   d) O dobro do número digitado
+   e) A metade do número digitado
 
+ * @author Miguel
+ */
 public class Metodos {
 
 	public int antecessor(int digitado) {
