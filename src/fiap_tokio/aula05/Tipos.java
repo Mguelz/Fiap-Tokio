8737913e7397dd408c2aac0d595dc532b603030b
Miguel <945507756m@gmail.com>
2023-04-18 16:36:51 -0300

    typcast

---
+++ src/fiap_tokio/aula05/Tipos.java
@@ -0,0 +1,33 @@
+package fiap_tokio.aula05;
+
+import java.util.Scanner;
+
+public class Tipos {
+
+	public static void main(String[] args) {
+
+		float preco = 10.99f;
+		float preco2 = (float) 12.90;
+
+		System.out.println("float 1 " + preco);
+		System.out.println("float 2 " + preco2);
+
+		int precoInteiro = (int) 2.50;
+		System.out.println("Valor Inteiro " + precoInteiro);
+
+		int numeroX = 6;
+		double numeroXDouble = numeroX;
+		System.out.println(" ");
+		System.out.println(numeroX);
+		System.out.println(numeroXDouble);
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("");
+		System.out.print("Digite um numero: ");
+		int numeroIn = (int) entrada.nextDouble();
+
+		System.out.println(numeroIn);
+	}
+
+}
