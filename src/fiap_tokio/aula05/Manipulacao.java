a0a5b2d50a859db276c1b041bf2c2d8564a2fd08
Miguel <945507756m@gmail.com>
2023-04-18 18:03:56 -0300

    exemplos aula 5

---
+++ src/fiap_tokio/aula05/Manipulacao.java
@@ -0,0 +1,33 @@
+package fiap_tokio.aula05;
+
+import java.util.Scanner;
+
+public class Manipulacao {
+
+	public static void main(String[] args) {
+
+		double salario = 1300.00;
+		String salarioStr = Double.toString(salario);
+		System.out.println(salarioStr);
+
+		System.out.println("--------------------------------------");
+
+		String idade = "25";
+		int idadeConvertida = Integer.parseInt(idade);
+		System.out.println("sua idade é: " + idadeConvertida);
+//		System.out.println("tipo da variavel: " + idadeConvertida.getClass().getSimpleName()); // isso se caso o int for Integer
+																							   // como pegar o tipo que esta variavel é (double, int, byte, etc)
+		System.out.println("--------------------------------------");
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("digite o preco do produto");
+		String preco = scanner.nextLine();
+
+		float precoConvertido = Float.parseFloat(preco);
+		System.out.println("Preco convertido: " + precoConvertido);
+
+		scanner.close();
+
+	}
+
+}
---
+++ src/fiap_tokio/aula05/Operadores.java
@@ -0,0 +1,50 @@
+package fiap_tokio.aula05;
+
+public class Operadores {
+	public static void main(String[] args) {
+
+		float num1 = 5f;
+		float num2 = 2f;
+
+		// soma
+		float soma = num1 + num2;
+		System.out.println(soma);
+
+		// subtração
+		float sub = num1 - num2;
+		System.out.println(sub);
+
+		// multiplicação
+		float multi = num1 * num2;
+		System.out.println(multi);
+
+		// divisão
+		float div = num1 / num2;
+		System.out.println(div);
+
+		// resto/módulo
+		float resto = num1 % num2;
+		System.out.println(resto);
+
+		System.out.println("-----------------------------------------------------------");
+
+		// operadores unários
+		int numero = 15;
+		System.out.println(numero);
+
+//		numero++;
+		System.out.println(numero++);
+		System.out.println(++numero);
+		System.out.println(numero);
+		System.out.println(numero + 1);
+		System.out.println(numero);
+		System.out.println(--numero);
+
+		System.out.println("-----------------------------------------------------------");
+		// operador de atribuição
+		int number = 5;
+		number += 5;
+		System.out.println(number);
+
+	}
+}
