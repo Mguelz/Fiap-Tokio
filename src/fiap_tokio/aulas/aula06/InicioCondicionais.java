9d11ac7d4883e5ed50c702c09eb3452099d5dfd9
Miguel <945507756m@gmail.com>
2023-04-19 18:21:42 -0300

    aula 6

---
+++ src/fiap_tokio/aulas/aula06/InicioCondicionais.java
@@ -0,0 +1,13 @@
+package fiap_tokio.aulas.aula06;
+
+public class InicioCondicionais {
+	public static void main(String[] args) {
+		// estrutura condicional simples
+		float nota = 5;
+		
+		if(nota < 6) {
+			System.out.println("sua nota é nota " + nota);
+		}
+		
+	}
+}
