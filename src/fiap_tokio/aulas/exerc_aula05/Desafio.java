4c2933319dac6407628a3e74e1a9bcaf5babc9e9
Miguel <945507756m@gmail.com>
2023-04-19 17:54:27 -0300

    desafio incompleto

---
+++ src/fiap_tokio/aulas/exerc_aula05/Desafio.java
@@ -0,0 +1,37 @@
+package fiap_tokio.aulas.exerc_aula05;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Faça o cálculo contrário do último exercício (Exercício 10): ▪ Leia um valor
+ * inteiro correspondente à idade de uma pessoa em dias e informe-a em anos,
+ * meses e dias. Também considere todo ano com 365 dias e todo mês com 30 dias.
+ * 
+ * @author logonrmlocal
+ *
+ */
+public class Desafio {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int qtdDias = 0;
+		int diasTotais = 0, anos = 0, meses = 0, dias = 0;
+
+		System.out.println("Digite quantos dias de vida voce tem");
+		qtdDias = scanner.nextInt();
+
+		System.out.println("");
+
+		qtdDias += 365;
+		anos += qtdDias / 365;
+		if (qtdDias < 365) {
+			qtdDias += 30;
+		} else if (qtdDias < 30) {
+			qtdDias += diasTotais;
+		}
+
+		System.out.println("Voce tem " + anos + " anos " + meses + " meses," + dias + " dias");
+		scanner.close();
+	}
+}
