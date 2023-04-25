acc532a6a1cb7b34552bbc2bbbf06ff3b2317a3e
Miguel <945507756m@gmail.com>
2023-04-25 18:25:16 -0300

    terminar exercicio 10 desafio

--- src/fiap_tokio/exercicios/aula07/Exercicio10Desafio.java
+++ src/fiap_tokio/exercicios/aula07/Exercicio10Desafio.java
@@ -17,24 +17,50 @@ import java.util.Scanner;
  *
  */
 public class Exercicio10Desafio {
-
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		
-		boolean primo = false;
 
-		System.out.println("calcular se um numero é primo no intervalo de 2 a 2000");
-		for (int i = 2; i < 2000; i++) {
-			if (i % 1 == 0 && i % i == 0) {
-				primo = true;
-			}
-			if (primo) {
-				System.out.println("O numero " + i + " é um numero primo");
-			}
-		}
+		// depois completa essa parte
+//		System.out.println("calcular se um numero é primo no intervalo de 2 a 2000");
+//		for (int i = 2; i < 2000; i++) {
+//
+//			for (int j = 2; j < 17; j++) {
+//				if ((i % j <= j) && i % j != 0) {
+//					primo = true;
+//				}
+//			}
+//
+//			if (primo) {
+//				System.out.println("O numero " + i + " é um numero primo");
+//			}
+//		}
 
 		scanner.close();
 
 	}
 
+	
+	public boolean primo() {
+		boolean primo = false;
+		int qtd = 0;
+
+		System.out.println("Digite um numero primo");
+		int num = scanner .nextInt();
+
+		for (int i = 1; i <= num; i++) { // somente resto por 1 e por ele mesmo tem que dar 0
+			if (num % i == (int) 0) {
+				qtd++;
+			}
+		}
+
+		if (qtd == 2) {
+			primo = true;
+		}
+
+		if (primo) {
+			System.out.println("O numero " + num + " eh um numero primo");
+		}
+		return primo;
+	}
 }
