dff34e2c4bb29aa81f177def07d7a5f6e6657873
Miguel <945507756m@gmail.com>
2023-05-02 16:58:47 -0300

    aula 11

---
+++ src/fiap_tokio/aulas/aula11/Operacoes.java
@@ -0,0 +1,9 @@
+package fiap_tokio.aulas.aula11;
+
+public class Operacoes {
+	
+	public static int somar(int a, int b) {
+		return a + b;
+	}
+	
+}
---
+++ src/fiap_tokio/aulas/aula11/ProgramaTop.java
@@ -0,0 +1,13 @@
+package fiap_tokio.aulas.aula11;
+
+public class ProgramaTop {
+
+	public static void main(String[] args) {
+		
+		int retorno = Operacoes.somar(10, 15);
+		
+		System.out.println(retorno);
+
+	}
+
+}
